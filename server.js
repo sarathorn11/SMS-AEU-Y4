969d12754a65bb5187040d0b43aee75c3aa05b50
Khy PHAT <khy.phat@axivit.com>
2025-01-11 11:41:25 +0700

    feat(auth): #8 disable auth migrate when run server

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 import app from './src/app.js'
-import db from './src/models/index.js'
-const { sequelize } = db
 const PORT = process.env.PORT || 3000
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`, `http://localhost:${PORT}`)
-  })
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`, `http://localhost:${PORT}`)
 })
